755d8da153fbe500920b6a30cb211dd2ef1a55e2
B-Mugnol <bruno_mugnol@hotmail.com>
2023-08-05 14:01:18 -0300

    Fix segfault on HumanB::attack() when no weapon is set

--- Module_01/ex03/HumanB.cpp
+++ Module_01/ex03/HumanB.cpp
@@ -6,19 +6,22 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:16:27 by bmugnol-          #+#    #+#             */
-/*   Updated: 2023/07/24 17:51:50 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2023/08/05 13:58:30 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string const name) : _name(name) { return; }
+HumanB::HumanB(std::string const name) : _name(name), _weapon(NULL) { return; }
 
 HumanB::~HumanB(void) { return; }
 
 void HumanB::attack(void) const
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (!this->_weapon)
+		std::cout << this->_name << " is unarmed and refuses to attack!" << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 	return;
 }
 
